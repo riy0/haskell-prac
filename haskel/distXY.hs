84e220796cc24da7d9263b6d42a54fe443b022da
Ryosuke Mishima <chac0ptx@gmail.com>
2016-10-15 10:05:36 +0900

    sqrt

---
+++ haskel/distXY.hs
@@ -0,0 +1,6 @@
+px,py :: Double
+px =9
+py =5
+
+dist0 :: Double - Double -> Double
+dist0 x y = sqrt $ x^2 + y^2
--- haskel/divide.hs
+++ haskel/divide.hs
@@ -1,3 +1,3 @@
 divide :: Double -> Double -> Double
-divide 0 b = b
-divide a b = b /a
+divide 0 = const 0
+divide a = (/a)
