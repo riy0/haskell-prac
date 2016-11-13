5503ba56ad64f6db411f5e7b51ed9aaa87df47de
Ryosuke Mishima <chac0ptx@gmail.com>
2016-11-13 11:30:37 +0900

    xy

--- haskel/montecarlo.hs
+++ haskel/montecarlo.hs
@@ -1,2 +1,6 @@
 randomXYs :: Int -> ([Double],[Double])
-randomXYs n = let (g,g') = split $ mkStdGen n in (randomRs(-1,1) g, ransomRs(-1,1) g')
+randomXYs n = let (g,g') = split $ mkStdGen n in 
+	(randomRs(-1,1) g, ransomRs(-1,1) g')
+points :: Int -> ([Double],[Double])
+points n = let (xs,ys') =  randomXYs n in zip xs ys
+
