97067d158bbd53bf126df5e6836698b5e0905a3f
Ryosuke Mishima <chac0ptx@gmail.com>
2016-11-13 11:35:44 +0900

    get point

--- haskel/montecarlo.hs
+++ haskel/montecarlo.hs
@@ -1,6 +1,17 @@
+import System.Random 
+
 randomXYs :: Int -> ([Double],[Double])
 randomXYs n = let (g,g') = split $ mkStdGen n in 
 	(randomRs(-1,1) g, ransomRs(-1,1) g')
+
 points :: Int -> ([Double],[Double])
 points n = let (xs,ys') =  randomXYs n in zip xs ys
 
+inCircle::(Double, Double) -> Bool
+inCircle (x,y) = x^2 + y^2 <= 1 
+
+inCirclePoints ::Int -> Int ->[(Double, Double)]
+inCirclePoints g n = filter inCircle . take $ points g 
+
+guessPi:: Int -> Int -> Double
+guessPi g n = 4 * from Integral( length $ inCirclePoints g n) / fromIntegral n
