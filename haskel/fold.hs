b5a26dbb267235a28ee132bc419784f10c53a6e5
Ryosuke Mishima <chac0ptx@gmail.com>
2016-11-20 13:07:49 +0900

    fldr

---
+++ haskel/fold.hs
@@ -0,0 +1,23 @@
+import Funpaala
+mySum [] = 0
+mySum(x:xs) = x + mySum xs
+
+myProduct[] = 1
+myProduct(x:xs) = x* myProduct xs
+
+(fldr op v) [] = v
+(fldr op v) (x:xs) = x`op` (fldr op v) xs  
+
+len :: a -> Int -> Int
+len = const (1 +)
+
+mySum, myProduct :: [Integer] -> Integer
+mySum = fldr (+) 0
+myProduct = fldr (*) 1
+
+myLength::[a]->Int
+myLength = fldr (const(1+))0
+
+myFldr :: (a -> b -> b) -> b -> [a] -> b
+myFldr op v (x :xs) = x `op` myFldr op v xs
+myFldr _  v [] = v
