a6415284be35c4c139690bbe2b592fcaa42d8f90
Ryosuke Mishima <chac0ptx@gmail.com>
2016-12-18 23:05:32 +0900

    point

---
+++ haskel/cpt.hs
@@ -0,0 +1,13 @@
+type Cartesian =(Double, Double)
+mulC :: Cartesian -> Double -> Cartesian
+mulC(x,y) n = (x*n, y*n)
+
+point1 :: Cartsian
+point1 =(8,5)
+
+type Polar =(Double, Double)
+mulP ::Polar -> Double -> Polar
+mulP (Polar d r) n = (d * n, r)
+
+point2 :: Polar 
+point2 = Polar 10 (pi /6)
